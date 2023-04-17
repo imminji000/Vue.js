2a37321b5fd6475b9e273dc408e0085a2795438c
imminji000 <swlss79@naver.com>
2023-04-17 12:03:19 +0900

    2023/04/17 수정

--- Ch07/src/components/sub3/LoginForm.vue
+++ Ch07/src/components/sub3/LoginForm.vue
@@ -20,24 +20,25 @@
 </template>
 <script setup>
 import { useRouter } from "vue-router";
+import { useStore } from "vuex";
 import { reactive } from "vue";
 import axios from "axios";
 const router = useRouter();
+const store = useStore();
 const user = reactive({
   uid: "",
   pass: "",
 });
 const loginProc = function () {
-  axios
-    .post("http://localhost:8080/Voard/user/login", user)
+  // 로그인 요청
+  store
+    .dispatch("login", user)
     .then((response) => {
-      console.log(response);
-      const token = response.data.accessToken;
-      localStorage.setItem("accessToken", token);
+      // 로그인 성공 전환
       router.push("/jwt/loginSuccess");
     })
     .catch((error) => {
-      console.log(error);
+      alert("로그인 실패!");
     });
 };
 </script>
