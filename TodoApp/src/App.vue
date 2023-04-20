db35786791ead471807da21a91e02526dc7b9abf
imminji000 <swlss79@naver.com>
2023-04-20 11:42:47 +0900

    2023/04/20

---
+++ TodoApp/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <Header v-on:addTodo="addTodoHandler"></Header>
+  <Main></Main>
+  <Footer></Footer>
+</template>
+<script>
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
+import { useStore } from "vuex";
+export default {
+  name: "App",
+  components: {
+    Header,
+    Main,
+    Footer,
+  },
+  setup() {
+    const store = useStore();
+    const addTodoHandler = (value) => {
+      // vuex store 저장
+      store.dispatch("addTodo", value);
+    };
+    return { addTodoHandler };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ TodoApp/src/components/Item.vue
@@ -0,0 +1,17 @@
+<template>
+  <li>
+    <i class="fa fa-check" aria-hidden="true"></i>
+    공부하기
+    <button>삭제</button>
+  </li>
+</template>
+<script>
+export default {
+  name: "Item",
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
